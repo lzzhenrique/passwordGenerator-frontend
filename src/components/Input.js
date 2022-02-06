8453b9a28a6c2d2f87a579efef2f60106ca3d822
Luiz Henrique <luishenriqueee2010@hotmail.com>
2022-02-06 16:46:44 -0300

    add: connection frontend-backend

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
-const options = ['Numbers', 'Lowercase', 'Simbols', 'Uppercase'];
+const options = ['numbers', 'lowerCase', 'simbols', 'upperCase'];
 
-const Input = ({ type, className }) => {
+const formatTitles = (name) => {
+  const lowerAll = name.toLowerCase().slice(1);
+  const upperFirst = name.charAt(0).toUpperCase();
+  
+  return `${upperFirst}${lowerAll}`;
+}
+
+const Input = ({ type, className, handleChange }) => {
   return options.map((option) => {
     return(
-      <div className={className}>
-        <label htmlFor={option}>
-          {`${option}`}
-          <input 
-            id={option}
-            type={type}
-            name={option}></input>
+      <div
+        className={ className }
+        key={ option }
+      >
+        <label htmlFor={ option }>
+          { formatTitles(option) }
+          <input
+            id={ option }
+            type={ type }
+            name={ option }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
       </div>
     );
--- src/pages/encript.js
+++ src/pages/encript.js
@@ -1,44 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Input from '../components/Input';
 import Options from '../components/Options.js'
+import http from '../services/api';
 
 export default function Encript() {
-  // const [form, setForm] = useState({
-  //   numbers: false,
-  //   lower: false,
-  //   passwordLength: 5,
-  //   simbols: false,
-  //   upper: false
-  // });
-  // const [btnDisable, setBtnDisable] = useState(true);
+  const [passwordOptions, setpasswordOptions] = useState({
+    passwordLength: null,
+    numbers: false,
+    simbols: false,
+    upperCase: false,
+    lowerCase: false,
+  });
+  const [button, setButton] = useState(true);
+  const [password, setPassword] = useState('');
+  const [isCopied, setisCopied] = useState(false);
+
+  
+
+  function handleChange({ target: { name, type, checked, value } }) {
+    const inputValue = type === 'checkbox' ? checked : value;
+    
+    setpasswordOptions({ ...passwordOptions, [name]: inputValue});
+  }
+
+  async function sendPasswordOptions () {
+    const { finalPassword } = await http.createPassword(passwordOptions);
+
+    setPassword(finalPassword);
+  }
+
+  async function copyToClipboard () {
+    await navigator.clipboard.writeText(password);
+    setisCopied(true);
+  }
+
+
+  useEffect(() => {
+    const { 
+      numbers,
+      lower,
+      simbols,
+      upper,
+      passwordLength
+    } = passwordOptions;
+  
+    if(passwordLength && (numbers || lower || simbols || upper)) {
+      return setButton(false);
+    }
+    if(!numbers && !lower && !simbols && !upper) {
+      return setButton(true);
+    }
+  }, [passwordOptions]);
 
   return (
     <div className='child'>
-      <h1 className='title'>Gere sua senha!</h1>
+      <h1 className='title'>Make your password!</h1>
       <div className='form'>
         <form>
           <div>
             <p>Password Length:
-              <select 
-                className='passwordLength' name="passwordLength">
+              <select
+                onChange= { handleChange }
+                className='passwordLength' name="passwordLength"
+              >
                 <Options/>
               </select>
             </p>
           </div>
           <div className='checkbox'>
             <Input
+              handleChange= { handleChange }
               type="checkbox"
               className="checks"
             />
           </div>
           <button
             type="button"
-            disabled
+            disabled={ button }
+            onClick={ () => sendPasswordOptions() }
           >
-            Gerar senha!
+            Make your password!
           </button>
         </form>
-    </div>
+        <div>
+          <input 
+            type="text"
+            className='ready-password-input'
+            value={ password }
+            readOnly
+            placeholder='Your password will appear here'
+            />
+          <button
+            type="button"
+            className='ready-password-button'
+            onClick={ () => copyToClipboard() }
+          >
+            <span>{ isCopied ? 'Copied!' : 'Copy' }</span>
+          </button>
+        </div>
+      </div>
       </div>
   )
 }
